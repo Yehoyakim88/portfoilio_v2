2115c6e44f0f067c5236a2ee36b140ab21c18026
Yehoyakim88 <yehoyakim@mail.de>
2023-02-15 17:05:52 +0100

    burgermenu with dropdown

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,4 +9,19 @@ export class MenuComponent {
   scrollUp() {
     window.scrollTo(0, 0);
   }
+
+  openDropDownMenu() {
+    let dropdown:HTMLElement = document.getElementById('dropdown-menu') as HTMLElement;
+    let burger:HTMLElement = document.getElementById('burger-menu-image') as HTMLElement;
+    dropdown.classList.remove('d-none');
+    burger.classList.add('d-none');
+  }
+
+  closeDropDownMenu() {
+    let dropdown:HTMLElement = document.getElementById('dropdown-menu') as HTMLElement;
+    let burger:HTMLElement = document.getElementById('burger-menu-image') as HTMLElement;
+    dropdown.classList.add('d-none');
+    burger.classList.remove('d-none');
+
+  }
 }
